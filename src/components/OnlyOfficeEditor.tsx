47bfbfd37ae1f0fa705ec0cbd13221a72ee47507
FarrukhInseyab <farrukh.khan@inseyab.com>
2025-07-08 18:32:35 +0500

    update url

--- src/components/OnlyOfficeEditor.tsx
+++ src/components/OnlyOfficeEditor.tsx
@@ -219,7 +219,7 @@ const retryLoadWithBackoff = () => {
         width: '100%',
         height: '600px',
         documentType: 'word',
-        documentServerUrl: 'http://onlyoffice',
+        documentServerUrl: 'http://172.22.25.154:8082',
         document: {
           fileType: 'docx',
           key: key,
@@ -287,7 +287,7 @@ const retryLoadWithBackoff = () => {
       width: '100%',
       height: '600px',
       documentType: 'word',
-      documentServerUrl: 'http://onlyoffice',
+      documentServerUrl: 'http://172.22.25.154:8082',
       document: {
         fileType: 'docx',
         key: documentKey,
